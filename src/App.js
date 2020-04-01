a821c5c84578eb857ee7c2517a6001778eca1475
ilgizDashkin <79374811484i@gmail.com>
2020-04-02 00:00:18 +0500

    make backend and vk style view

--- src/App.js
+++ src/App.js
@@ -3,51 +3,124 @@ import React, { Component, useState, useEffect, } from 'react';
 // import View from '@vkontakte/vkui/dist/components/View/View';
 // import ScreenSpinner from '@vkontakte/vkui/dist/components/ScreenSpinner/ScreenSpinner';
 import '@vkontakte/vkui/dist/vkui.css';
-import { View, Panel, PanelHeader, FormLayout, File } from '@vkontakte/vkui';//пакеты из вк
+import { View, Panel, PanelHeader, FormLayout, File, Div, Button, Input, Spinner, CardGrid, Card } from '@vkontakte/vkui';//пакеты из вк
 import Icon24CameraOutline from '@vkontakte/icons/dist/24/camera_outline';
-
+import Icon24Send from '@vkontakte/icons/dist/24/send';
 
 
 class App extends Component {
 	constructor(props) {
 		super(props);
-		  this.state = {
-			selectedFile: null
-		  }
-	   
-	  }
-	  
-	onChangeHandler=event=>{
+		this.state = {
+			isLoading: false,
+			nameKl: '',
+			zamer: '',
+			where: '',
+			who: '',
+			selectedFile: null,
+			result_serv: null
+		}
+
+	}
+
+	componentDidMount() {
+		const lastState = localStorage.savepovkl
+		if (lastState) {
+			// console.log(lastState)
+			this.setState(JSON.parse(lastState))
+		}
+	}
+	//обязательно используем стрелочные фунции чтоб не прописывать методы в конструкторе
+	nameKlChange = (event) => {
+		this.setState({ nameKl: event.target.value.toUpperCase() });
+	}
+	zamerChange = (event) => {
+		this.setState({ zamer: event.target.value });
+	}
+	whereChange = (event) => {
+		this.setState({ where: event.target.value.toUpperCase() });
+	}
+	whoChange = (event) => {
+		this.setState({ who: event.target.value.toUpperCase() });
+	}
+	onChangeHandler = event => {
 		// console.log(event.target.files[0])
 		this.setState({
-			selectedFile: event.target.files[0],
-			loaded: 0,
-		  })
+			selectedFile: event.target.files			
+		})
 	}
-	onClickHandler = () => {
-		const data = new FormData() 
-		data.append('file', this.state.selectedFile)
+	onClickHandler = async () => {
+		const data = new FormData()
+		// data.append('foto1', this.state.selectedFile)
+		if ((this.state.selectedFile) && (this.state.selectedFile.length <= 3)) {
+			this.setState({ isLoading: true }) //пока грузится показываем спинер
+			for (let x = 1; x <= this.state.selectedFile.length; x++) {
+				data.append('foto' + x, this.state.selectedFile[x - 1])
+			}
+			// data.append('foto1', this.state.selectedFile[0])
+			data.append('name', this.state.nameKl)
+			data.append('zamer', this.state.zamer)
+			data.append('otkuda', this.state.where)
+			data.append('kto', this.state.who)
+			let response = await fetch('https://ilgiz.h1n.ru/savepovkl.php', {
+				method: 'POST',
+				body: data
+			});
+
+			let result = await response.json()
+			this.setState({
+				isLoading: false,
+				result_serv: result,
+			})
+			// console.log(result);
+			localStorage.savepovkl = JSON.stringify(this.state);//сохраняем стейт в локалсторадже
+		} else {
+			alert('пожалуйста выберите от 1 до 3 фото')
+		}
 	}
 
 	render() {
 		return (
-			// <View activePanel={activePanel} popout={popout}>
-			// 	<Home id='home' fetchedUser={fetchedUser} go={go} />
-			// 	<Persik id='persik' go={go} />
-			// </View>
 			<View id="view" activePanel="panel">
 				<Panel id="panel">
 					<PanelHeader>добавить привязку КЛ</PanelHeader>
-					<div className="container">
-						<div className="row">
-							<div className="col-md-6">				
-									{/* <div className="form-group files">
-										<label>Upload Your File </label>
-										<input type="file" className="form-control" onChange={this.onChangeHandler}/>
-									</div> */}
-									<File onChange={this.onChangeHandler}/><p>{this.state.selectedFile?this.state.selectedFile.name:null}</p>
-							<button className="btn btn-danger btn-lg btn-block" onClick={this.onClickHandler}>send</button>	
-							</div>							
+					<div className="container bg-dark text-center ">
+						<div className='container p-2'>
+							<FormLayout align="center">
+								<Input type="text" top="наименование КЛ" placeholder='введите название КЛ' align="center" value={this.state.nameKl} onChange={this.nameKlChange} />
+								<Input type="number" top="замер" placeholder='введите замер' align="center" value={this.state.zamer} onChange={this.zamerChange} />
+								<Input type="text" top="откуда замер" placeholder='введите откуда замер' align="center" value={this.state.where} onChange={this.whereChange} />
+								<Input type="text" top="кто искал" placeholder='введите кто искал' align="center" value={this.state.who} onChange={this.whoChange} />
+								<File multiple onChange={this.onChangeHandler} top="фото места повреждения" before={<Icon24CameraOutline />} size="l">
+									можно выбрать не более 3-х фото
+                                </File>
+								<p className=" text-white">
+									количество выбранных фото:
+									{
+										this.state.selectedFile ?
+											(this.state.selectedFile.length > 3 ? <span className='bg-danger'> больше 3</span> : ` ${this.state.selectedFile.length||''}`)
+											: null
+									}
+								</p>
+								<Button onClick={this.onClickHandler} before={<Icon24Send />} size="l">отправить</Button>
+								{
+									this.state.isLoading ?
+										<div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+											<Spinner size="large" style={{ marginTop: 20 }} />
+										</div> :
+										<CardGrid>
+											<Card size="l" mode="outline">
+												{
+													this.state.result_serv ?
+														`привязка КЛ ${this.state.result_serv.name} сохранена.
+								                    ${this.state.result_serv.file1} ${this.state.result_serv.file2 || ''} ${this.state.result_serv.file3 || ''}
+								                    ${this.state.result_serv.file1geo} Спасибо за работу! :)` :
+														null
+												}
+											</Card>
+										</CardGrid>
+								}
+							</FormLayout>
 						</div>
 					</div>
 				</Panel>
---
+++ src/backend/savepovkl.php
@@ -0,0 +1,229 @@
+<?php header('Access-Control-Allow-Origin: *'); //разрешаем кроссдоменные запросы CORS  
+?> 
+<?php
+require_once 'connection.php'; // подключаем скрипт
+ 
+if(isset($_POST['name']) /*&& isset($_POST['priv'])*/){
+    $json = array();//переменная для ответа от сервера
+    // подключаемся к серверу
+    $link = mysqli_connect($host, $user, $password, $database) 
+        or die("Ошибка " . mysqli_error($link)); 
+     
+    // экранирования символов для mysql
+    $name = htmlentities(mysqli_real_escape_string($link, $_POST['name']));
+   // $date = htmlentities(mysqli_real_escape_string($link, $_POST['date']));
+    $zamer = htmlentities(mysqli_real_escape_string($link, $_POST['zamer']));
+     $otkuda = htmlentities(mysqli_real_escape_string($link, $_POST['otkuda']));
+    $priv = htmlentities(mysqli_real_escape_string($link, $_POST['priv']));
+    $dlinna = htmlentities(mysqli_real_escape_string($link, $_POST['dlinna']));
+    @$foto1 = htmlentities(mysqli_real_escape_string($link, $_POST['foto1']));
+    @$foto2 = htmlentities(mysqli_real_escape_string($link, $_POST['foto2']));
+    @$foto3 = htmlentities(mysqli_real_escape_string($link, $_POST['foto3']));
+     $kto = htmlentities(mysqli_real_escape_string($link, $_POST['kto']));
+    //$gps = htmlentities(mysqli_real_escape_string($link, $_POST['gps']));
+    
+   
+    //file
+  
+    if ($_FILES && $_FILES['foto1']['error']== UPLOAD_ERR_OK && $_FILES['foto1']['name']!= '')
+{
+    $name_foto1 = 'foto/'.$_FILES['foto1']['name'];
+    move_uploaded_file($_FILES['foto1']['tmp_name'], $name_foto1);
+    // echo "Файл 1 загружен. ";
+    $json['file1']="Файл 1 загружен. ";
+    //поиск даты в фото
+    $filename=$name_foto1;
+    $exif = exif_read_data($filename, 'IFD0');
+    // echo $exif===false ? "Не найдено данных заголовка в фото1 . " : "Изображение содержит заголовки в фото1 . ";
+    $json['file1exif']=($exif===false ? "Не найдено данных заголовка в фото1 . " : "Изображение содержит заголовки в фото1 . ");
+    $edate = $exif['DateTime'];
+    // echo "Дата из фото $edate. ";
+    $date = substr($edate, 0, 10); // возвращает "10 первых символов даты"
+//это скрипт по извлечению gps
+// Имя файла для обработки
+$file_path=$filename; 
+// Массив с GPS-данными
+$gps_data=array(); 
+$f=fopen($file_path,'r');
+$tmp=fread($f, 2);
+if ($tmp==chr(0xFF).chr(0xD8)) {
+  $section_id_stop=array(0xFFD8,0xFFDB,0xFFC4,0xFFDD,0xFFC0,0xFFDA,0xFFD9);
+  while (!feof($f)) {
+    $tmp=unpack('n',fread($f,2));
+    $section_id=$tmp[1];
+    $tmp=unpack('n',fread($f,2));
+    $section_length=$tmp[1];
+    // Началась секция данных, заканчиваем поиск
+    if (in_array($section_id, $section_id_stop)) {
+        break;
+    }
+ 
+    // Найдена EXIF-секция
+    if ($section_id==0xFFE1) {
+      $exif=fread($f,($section_length-2));
+ 
+      // Это действительно секция EXIF?
+      if (substr($exif,0,4)=='Exif') {
+        // Определить порядок следования байт
+        switch (substr($exif,6,2)) {
+          case 'MM': {
+            $is_motorola=true;
+            $mask1='n';
+            $mask2='N';
+            break;
+          }
+          case 'II': {
+            $is_motorola=false;
+            $mask1='v';
+            $mask2='V';
+            break;
+          }
+        }
+        // Количество тегов
+        $tmp=unpack($mask2,substr($exif,10,4));
+        $offset_tags=$tmp[1];
+        $tmp=unpack($mask1,substr($exif,14,2));
+        $num_of_tags=$tmp[1];
+ 
+        if ($num_of_tags==0) { return true; }
+ 
+        $offset=$offset_tags+8;
+ 
+        // Поискать тег GPSInfo
+        for ($i=0; $i<$num_of_tags; $i++) {
+          $tmp=unpack($mask1,substr($exif,$offset,2));
+          $tag_id=$tmp[1];
+          $tmp=unpack($mask2,substr($exif,$offset+8,4));
+          $value=$tmp[1];
+ 
+          $offset+=12;
+ 
+          // GPSInfo
+          if ($tag_id==0x8825) {
+            $gps_offset=$value+6;
+            // Количество GPS-тегов
+            $tmp=unpack($mask1,substr($exif,$gps_offset,2));
+            $num_of_gps_tags=$tmp[1];
+ 
+            $offset=$gps_offset+2;
+ 
+            if ($num_of_gps_tags>0) {
+              // Обработка GPS-тегов
+              for ($i=0; $i<$num_of_gps_tags; $i++) {
+                $tmp=unpack($mask1,substr($exif,$offset,2));
+                $tag_id=$tmp[1];
+                $tmp=unpack($mask2,substr($exif,$offset+8,4));
+                $value=$tmp[1];
+ 
+                // GPSLatitudeRef или GPSLongitudeRef
+                if ($tag_id==0x0001 || $tag_id==0x0003) {
+                  $tmp=unpack('V',substr($exif,$offset+8,4));
+                  $value=$tmp[1];
+                  if ($value!=0) {
+                    if ($tag_id==0x0001) {
+                      $gps_data['GPSLatitudeRef']=chr($value);
+                    }
+                    else {
+                      $gps_data['GPSLongitudeRef']=chr($value);
+                    }
+                  }
+                }
+                // GPSLatitude или GPSLongitude
+                if ($tag_id==0x0002 || $tag_id==0x0004) {
+                  $rational_offset=$value+6;
+                  $tmp=unpack($mask2,substr($exif,$rational_offset+4*0,4));
+                  $val1=$tmp[1];
+                  $tmp=unpack($mask2,substr($exif,$rational_offset+4*1,4));
+                  $div1=$tmp[1];
+                  $tmp=unpack($mask2,substr($exif,$rational_offset+4*2,4));
+                  $val2=$tmp[1];
+                  $tmp=unpack($mask2,substr($exif,$rational_offset+4*3,4));
+                  $div2=$tmp[1];
+                  $tmp=unpack($mask2,substr($exif,$rational_offset+4*4,4));
+                  $val3=$tmp[1];
+                  $tmp=unpack($mask2,substr($exif,$rational_offset+4*5,4));
+                  $div3=$tmp[1];
+                  if ($div1!=0 && $div2!=0 && $div3!=0) {
+                    $tmp=round(($val1/$div1+$val2/$div2/60+$val3/$div3/3600),6);
+                    if ($tag_id==0x0002) {
+                      $gps_data['GPSLatitude']=$tmp;
+                    }
+                    else {
+                      $gps_data['GPSLongitude']=$tmp;
+                    }
+                  }
+                }
+                // GPSSatellites
+                if ($tag_id==0x0008) {
+                  $tmp=intval(substr($exif,$offset+8,4));
+                  if ($tmp>0) {
+                    $gps_data['GPSSatellites']=$tmp;
+                  }
+                }
+ 
+                $offset+=12;
+              }
+            }
+            break;
+          }
+        }
+      }
+    }
+    else {
+      // Пропустить секцию
+      fseek($f, ($section_length-2), SEEK_CUR);
+    }
+    // Тег GPSInfo найден
+    if (count($gps_data)!=0) { break; }
+  }
+}
+fclose($f);
+// Данные GPS
+if (!$gps_data['GPSLatitude']=="")
+    {
+     $text_coordinate=$gps_data['GPSLatitude'].",".$gps_data['GPSLongitude'];
+     $gps=$text_coordinate;
+    //   echo "Координаты $gps . ";
+    $json['file1geo']="Координаты $gps . ";
+    }
+//  else {echo "Нет геотегов в фото $gps . ";}
+else {$json['file1geo']= "Нет геотегов в фото $gps . ";}
+//конец скрипта по извлечению gps
+}
+   if ($_FILES && $_FILES['foto2']['error']== UPLOAD_ERR_OK && $_FILES['foto2']['name']!= '')
+{
+    $name_foto2 = 'foto/'.$_FILES['foto2']['name'];
+    move_uploaded_file($_FILES['foto2']['tmp_name'], $name_foto2);
+    // echo "Файл 2 загружен. ";
+    $json['file2']="Файл 2 загружен. ";
+}  
+ if ($_FILES && $_FILES['foto3']['error']== UPLOAD_ERR_OK && $_FILES['foto3']['name']!= '')
+{
+    $name_foto3 = 'foto/'.$_FILES['foto3']['name'];
+    move_uploaded_file($_FILES['foto3']['tmp_name'], $name_foto3);
+    // echo "Файл 3 загружен.  ";
+    $json['file3']="Файл 3 загружен. ";
+}     
+    
+   //fileend 
+   ini_set('display_errors', 'Off'); //отключить сообщение об ошибках
+     // создание строки запроса
+    $query ="INSERT INTO povkl VALUES(NULL,'$name','$date','$zamer','$otkuda','$priv','$dlinna','$gps','$kto','$name_foto1'"
+            . ",'$name_foto2','$name_foto3')";
+      ini_set('display_errors', 'On');  //включить сообщение об ошибках      
+     
+    // выполняем запрос
+    $result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link)); 
+    if($result)
+    {        
+        // echo "Спасибо ваша привязка КЛ $name добавлена! ";
+        
+       $json['name'] = $name;
+       echo json_encode($json);//отправляем ответ от сервер в json
+    }
+    // закрываем подключение
+    mysqli_close($link);
+}
+?>
+
+
